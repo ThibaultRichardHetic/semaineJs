7a00ca76ddbe1b4ee95e8fc64d0437b8d360a52d
charly33 <32869761+charly33@users.noreply.github.com>
2018-04-09 14:37:00 +0200

    premier jet

--- script/app.js
+++ script/app.js
@@ -1,1 +1,22 @@
- console.log("fdp");
+let word = ["charles","juleslapute","michel","rino","girafe"]
+let input = document.querySelector('input')
+let form = document.querySelector('form')
+let alea
+
+function aleaWord(array){
+ alea = parseInt(Math.floor(Math.random() * array.length))
+ document.getElementById("word").innerHTML = word[alea]
+
+}
+aleaWord(word)
+
+input.addEventListener(
+  'keyup',
+  function(e){
+    if(this.value.replace(' ','') == word[alea]) {
+      console.log('ok')
+      form.reset()
+      aleaWord(word)
+    }
+  }
+)
