b4a4c7e2b366ae85349464c5783afc2d3ca2c2dc
ThibaultRichardHetic <thibault.richard2000@gmail.com>
2018-04-09 16:41:22 +0200

    tous les mots

--- script/data.js
+++ script/data.js
@@ -1,1 +1,33 @@
 let word = ["charles","juleslapute","michel","rino","girafe"]
+
+//niveau 1
+
+const plaine = ["prairie", "mouton", "herbe", "ferme", "vache", "taureau", "poule", "oeufs", "arbre", "cheval", "fleuve", "rivière", "campagne", "rivière", "champ de blé", "paysage"]
+
+const savane = ["lion", "guépard", "gazelle", "gnou", "girafe", "eléphant", "zèbre", "rhinocéros", "antilope", "hyènes", "vautour", "alizés", "fougères", "baobab", "tigre", "hippopotame", "crocodile", "panthère", "buffle",
+"impala", "springbok", "oryx", "autruche", "phacochère", "chacal", "varan"]
+
+const plage = ["sable", "mer", "poisson", "crabe", "moule", "huître", "Château de sable", "parasol", "pelle", "pêcheur", "baignade", "houle", "sceau", "littoral", "écume", "blockhaus", "thon", "saumon", "cabillaud",
+"épave", "cerf-volant", "coquillages", "serviette", "crème", "solaire", "coup de soleil", "tong", "galet", "étoile de mer"
+"voilier", "mouette", "phare"]
+
+//niveau 2
+
+const nuage = ["aéronautique", "atmosphère", "providence", "condensation", "altocumulus", "septième ciel", "ouranos"]
+
+const jungle = ["sauvage", "amazonie", "biodiversité", "tropical", "fiévreux", "chaleur", "humidité", "maladie", "amazones", "aborigène"]
+
+const ville = ["immeuble", "gratte ciel", "monumental", "avenue", "boulevard", "véhicule", "métropolitain", "tramway"]
+
+const montagne = ["himalaya", "pyrénées", "yéti", "tellurique", "plateaux", "relief", "géomorphologie", "cirque glaciaire", "everest", "aiguille du midi"]
+
+//niveau 3
+
+const sousMarin = ["abysse", "torpille", "periscope", "océanographie", "poisson", "requins", "eau", "mer", "sonars", "bathyscaphe", "bathyscaphe", "schnorchel", "torpilleurs", "compas"]
+
+const glacial = ["glacier", "précipitations", "glace", "alpinisme", "ascension", "dégaines", "baudrier", "froid", "crevasse", "avalanche", "hypothermie", "yeti"]
+
+const volcan = ["magma", "lave", "terrestre", "explosion", "cendres", "fumée", "catastrophe naturel", "gaz volcanique", "eruption", "plinienne"]
+
+const galaxie = ["supernova", "voie lactée", "trou noir", "astronaute", "fusée", "étoile", "nébuleuses", "système solaire", "lumière", "ellipsoïde", "starburst", "andromède", "atmosphère", "interstellaire", "pluton", "jupiter", "téléscope", "espace", "singularité",
+"hawking", "univers", "big bang"]
